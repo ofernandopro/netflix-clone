6f4425ebf67bc5cb7f3f1cdd9bed0b1c24c90a85
Fernando Moreira <fernandoepm.pro@gmail.com>
2020-12-23 18:22:17 -0300

    Updating movie rows

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => { // Quando a tela for carregada, será executada a função dentro do useEffect
     const loadAll = async () => {
       // Pegando a lista TOTAL:
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // Pegando o Featured:
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+
+      console.log(chosen);
     }
 
     loadAll();
@@ -19,6 +28,11 @@ export default () => {
 
   return (
     <div className="page">
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} /> // O key é necessário em todo map que utilizo 
---
+++ src/components/FeaturedMovie/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+export default () => {
+  return (
+    <div>
+      Filme em destaque
+    </div>
+  )
+}
