4e373197b5b2bec61037fbbff3af80f8c27848a4
Fernando Moreira <fernandoepm.pro@gmail.com>
2020-12-23 18:48:43 -0300

    Building the style of the featured section

--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './styles.css';
 
-export default () => {
+export default ({ item }) => {
   return (
-    <div>
-      Filme em destaque
-    </div>
-  )
+    <section className="featured" style={{
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+    }}>
+      <div className="featured-vertical">
+        <div className="featured-horizontal">
+
+        </div>
+      </div>
+    </section>
+  );
 }
