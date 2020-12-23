bfa8700a9db2ec92d51a8824a36cf5cf6922376c
Fernando Moreira <fernandoepm.pro@gmail.com>
2020-12-23 16:05:19 -0300

    Adding the movies lists in the website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => { // Quando a tela for carregada, será executada a função dentro do useEffect
+    const loadAll = async () => {
+      // Pegando a lista TOTAL:
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+
   return (
-    <div>
-      Olá, Mundo!
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} /> // O key é necessário em todo map que utilizo 
+        ))}
+      </section>
     </div>
   );
 }
