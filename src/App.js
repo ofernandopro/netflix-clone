89da9ed5629fde1dee712d0b8820b364e1906c72
Fernando Moreira <fernandoepm.pro@gmail.com>
2020-12-24 18:25:35 -0300

    Adding Header Component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => { // Quando a tela for carregada, será executada a função dentro do useEffect
     const loadAll = async () => {
@@ -26,8 +28,26 @@ export default () => {
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 50) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
   return (
     <div className="page">
+
+      <Header black={blackHeader} />
 
       {featuredData &&
         <FeaturedMovie item={featuredData} />
@@ -38,6 +58,12 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items} /> // O key é necessário em todo map que utilizo 
         ))}
       </section>
+
+      <footer>
+        Feito com <span role="img" aria-label="coração">❤</span> por Fernando Moreira<br />
+        Direitos de imagem para Netflix<br />
+        Dados pêgos do site themoviedb.org
+      </footer>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+
+export default ({ black }) => {
+  return (
+    <header className={black ? 'black' : ''}>
+      <div className="header-logo">
+        <a href="/">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix" />
+        </a>
+      </div>
+      <div className="header-user">
+        <a href="/">
+          <img src="https://i.pinimg.com/originals/b6/77/cd/b677cd1cde292f261166533d6fe75872.png" alt="User" />
+        </a>
+      </div>
+    </header>
+  );
+}
